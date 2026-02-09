4c0bd5fca2bbc343f41596507aee11d15301cad5
johnedmonds-bell <john@edmonds-bell.co.uk>
2026-02-09 12:47:53 +0000

    Migrate from Jest to Vitest

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-// jest.config.js
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-    preset: 'ts-jest/presets/default-esm',
-    testEnvironment: 'node',
-    extensionsToTreatAsEsm: ['.ts'],
-    testMatch: ['**/*.test.ts', '**/*.test.js'],
-};
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/test/setup.ts',
+    },
     plugins: [
         react(),
         VitePWA({
